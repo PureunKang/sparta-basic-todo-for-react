6971bd98f5ba5ce0fd1b391c588de730afbe006b
PureunKang <pureun0000@gmail.com>
2025-01-23 14:43:13 +0900

    feat : 할일 추가 기능 구현

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
+
 const TodoList = () => {
-  const todos = [
+  const TASK_LIST = [
     { id: 1, text: "Buy milk" },
     { id: 2, text: "Clean the house" },
     { id: 3, text: "Go for a run" },
@@ -11,11 +13,43 @@ const TodoList = () => {
     { id: 9, text: "Do laundry" },
     { id: 10, text: "Write code" },
   ];
+
+  const [todos, setTodos] = useState(TASK_LIST);
+
+  const [task, setTask] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (!task.trim()) return alert("할 일을 입력해 주세요.");
+
+    const newTask = {
+      id: crypto.randomUUID(),
+      text: task,
+    };
+
+    setTodos([newTask, ...todos]);
+
+    setTask("");
+  };
+
+  const inputBoxHandler = (e) => {
+    setTask(e.target.value);
+  };
+
   return (
     <div className="todo-list">
-      {todos.map((n) => (
-        <li key={n.id}>{n.text}</li>
-      ))}
+      <form onSubmit={submitHandler}>
+        <div>
+          <input type="text" value={task} onChange={inputBoxHandler} />
+          <button type="submit">제출하기</button>
+        </div>
+        <div>
+          {todos.map((n) => (
+            <li key={n.id}>{n.text}</li>
+          ))}
+        </div>
+      </form>
     </div>
   );
 };
